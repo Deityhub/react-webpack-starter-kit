fd859ee6dd34da6e892fad01ff751ae2bbb01fc6
Ojini Chizoba Jude <judoc96@gmail.com>
2018-12-31 02:23:48 +0100

    initial files for starter kit

---
+++ src/components/app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./app.scss";
+import imgFile from "../../assests/image/Favicon.png";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+          React Page <img src={imgFile} width="30" height="30" />{" "}
+        </h1>
+        <br />
+        <a
+          className="button-line"
+          href="https://github.com/deityhub"
+          target="_blank"
+        >
+          Know more
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
